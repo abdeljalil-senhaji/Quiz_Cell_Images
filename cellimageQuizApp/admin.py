8d97aaebc76e165cf29a07f801fb50195b73585b
abdel <abdeljalil.senhajirachik@usmba.ac.ma>
2021-11-22 00:58:47 +0100

    readme

--- cellimageQuizApp/admin.py
+++ cellimageQuizApp/admin.py
@@ -5,11 +5,10 @@ from .forms import FormImage
 
 # Register your models here.
 
-#######################################################
-##### Edit Image DB directly in admin interface #######
-#######################################################
 class PersonAdmin(admin.ModelAdmin):
     form = FormImage
 
 
 admin.site.register(Image, PersonAdmin)
+
+
